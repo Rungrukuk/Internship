dcf884b00e666a911a345f7bada23bdce74b3a2e
Rungrukuk <70916892+Rungrukuk@users.noreply.github.com>
2023-04-27 14:59:13 +0400

    Debug Messages has been deleted

--- VolunteerProject/admin/create_user.php
+++ VolunteerProject/admin/create_user.php
@@ -55,7 +55,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
     if($usernameError == "" && $passwordError == "" && $nameError == "" && $statusError == "")
     {
-        echo "Yes";
         $sql = "INSERT INTO `users`(`name`, `username`, `password`, `status`) VALUES ('$name','$username','$password','$status')";
         if ($conn->query($sql) === TRUE) 
         {
--- VolunteerProject/admin/functions/connections.php
+++ VolunteerProject/admin/functions/connections.php
@@ -10,8 +10,7 @@ $conn = new mysqli($servername, $username, $password,$dbName);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-else
-  echo "Succesfully Connected";
+
 
 
 ?>
